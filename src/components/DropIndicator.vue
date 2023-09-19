e2b73c5570990bac32a33d7a4a0e9d5e3ed61f06
Chicote Belsonda <belsondachi@gmail.com>
2023-09-19 15:14:49 +0800

    Reverted drop indicator changes

--- src/components/DropIndicator.vue
+++ src/components/DropIndicator.vue
@@ -21,15 +21,30 @@ export default {
 
 <style lang='scss' scoped>
 .indicator {
-  width: 100%;
-  height: 100%;
-  border-radius: 4px;
-  border: #217ce8 2px solid;
+  width: 12px;
+  height: 12px;
+  border-radius: 60px;
+  background-color: #217ce8;
+  left: 50%;
+  bottom: -5px;
+  margin-top: -5px;
+  margin-left: -6px;
   opacity: 1;
   transition: all 0.3s cubic-bezier(0.05, 0.03, 0.35, 1);
   transform: scale(1);
   position: absolute;
   z-index: 2;
+
+  &:after {
+    content: '';
+    display: block;
+    width: 12px;
+    height: 12px;
+    background-color: #217ce8;
+    transform: scale(1.7);
+    opacity: 0.5;
+    border-radius: 60px;
+  }
 }
 
 .indicator .not-allowed {
